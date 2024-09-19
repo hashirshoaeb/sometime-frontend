ea4c472cef158552d9b055e31d2c2a2e1cd1ad93
hashirshoaeb <hashirshoaeb@gmail.com>
2024-09-19 14:37:50 +0500

    initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import packageJson from './package.json' assert { type: 'json' };
+
+const nextConfig = {
+  env: {
+    APP_VERSION: packageJson.version,
+  }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,50 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
+// https://stackoverflow.com/a/76919264/16220781
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/forms')({ strategy: 'class' }),
+  ],
+  daisyui: {
+    themes: ["light"]
+  }
 };
 export default config;
