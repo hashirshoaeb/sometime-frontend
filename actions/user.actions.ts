4e2e5a82268c20cc8b99870761def05d311bfe09
hashirshoaeb <hashirshoaeb@gmail.com>
2024-12-18 16:23:11 +0500

    mock apis added

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,29 +1,47 @@
+import { LoginUser } from "@/types/ResponseTypes";
 import { Exception } from "@/utils/exceptions";
 import { fetchWrapper } from "@/utils/fetchWrapper";
 
 
 // Action to send phone number
-export const sendPhoneNumber = async (phoneNumber: string) => {
-  try {
-    const response = await fetchWrapper.post({
-      endpoint: "auth-send",
-      data: { phoneNumber },
-    });
-    return response.response!;
-  } catch (error: any) {
-    throw new Exception(error);
-  }
-};
+export async function sendPhoneNumber(phoneNumber: string): Promise<LoginUser> {
+  // try {
+  //   const response = await fetchWrapper.post({
+  //     endpoint: "auth-send",
+  //     data: { phoneNumber },
+  //   });
+  //   return response.response!;
+  // } catch (error: any) {
+  //   throw new Exception(error);
+  // }
+  await new Promise(resolve => setTimeout(resolve, 1500));
+  return {
+    id: 1,
+    phone: phoneNumber,
+    isVerified: false,
+  };
+}
 
 // Action to verify phone number with code
-export const verifyPhoneNumber = async (phoneNumber: string, code: string) => {
-  try {
-    const response = await fetchWrapper.post({
-      endpoint: 'auth-verify',
-      data: { phoneNumber, code },
-    });
-    return response.response!;
-  } catch (error: any) {
-    throw new Exception(error);
+export async function verifyPhoneNumber(phoneNumber: string, code: string): Promise<LoginUser> {
+  // try {
+  //   const response = await fetchWrapper.post({
+  //     endpoint: 'auth-verify',
+  //     data: { phoneNumber, code },
+  //   });
+  //   return response.response!;
+  // } catch (error: any) {
+  //   throw new Exception(error);
+  // }
+  await new Promise(resolve => setTimeout(resolve, 1500));
+  const otp = "4321";
+  if (otp === code) {
+    return {
+      id: 1,
+      phone: phoneNumber,
+      isVerified: true
+    }
+  } else {
+    throw new Exception("Invalid OTP");
   }
-};
+}
